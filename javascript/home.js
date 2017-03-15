5059702f523a5cf3a4209b1a74578c87caf65e1c
Erik Enderlein <erik.end@gmail.com>
2017-03-15 12:04:40 -0500

    added similar functionality to both the about and contact buttons on the menu.

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,7 @@
 'use strict';
 var portFlag = false;
 var contactFlag = false;
+var aboutFlag = false;
 $(document).ready(function(){
   $('#portfolio').click(function(){
     if (portFlag === false){
@@ -19,5 +20,39 @@ $(document).ready(function(){
     $('#about').fadeToggle(1000);
     $('#contact').fadeToggle(1000);
     $('.portfolioPiece').slideToggle(1000);
+  });
+  $('#contact').click(function(){
+    if (contactFlag === false){
+      $('#contact').animate({
+        top: '-=50',
+        width: '400'
+      }, 1000 );
+      contactFlag = true;
+    } else {
+      $('#contact').animate({
+        top: '+=50',
+        width: '125'
+      }, 1000 );
+      contactFlag = false;
+    }
+    $('#about').fadeToggle(1000);
+    $('#portfolio').fadeToggle(1000);
+    $('#contactInfo').slideToggle(1000);
+  });
+  $('#about').click(function(){
+    if (aboutFlag === false){
+      $('#about').animate({
+        width: '400'
+      }, 1000 );
+      aboutFlag = true;
+    } else {
+      $('#about').animate({
+        width: '125'
+      }, 1000 );
+      aboutFlag = false;
+    }
+    $('#contact').fadeToggle(1000);
+    $('#portfolio').fadeToggle(1000);
+    $('#aboutInfo').slideToggle(1000);
   });
 });
