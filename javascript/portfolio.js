6e4db1e021f456dcff105fa54934174de852b6f8
Anna Shelby <bonanashelby@gmail.com>
2017-03-29 11:40:57 -0700

    peer programmed with Anna and Erik

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,54 +1,50 @@
 'use strict';
-var portObjectArray = [];
-var ports = {};
-function PortfolioObject(portData){
-  this.name = portData.name;
-  this.link = portData.link;
-  this.img = portData.img;
-  this.body = portData.body;
-  this.category = portData.category;
-  this.date = portData.date;
-}
-//
-// function portSetup(){
-//   for (var i = 0; i < portfolioData.length; i++) {
-//     portObjectArray.push(new PortfolioObject(portfolioData[i]));
-//   }
-// }
-// ports.renderToPage() = function(){
-//   portObjectArray.forEach(function(i) {
-//     $('#portfolio').append(i.toHtml());
-//   });
-// };
-PortfolioObject.prototype.toHtml = function(){
-  var source = $('#portfolio-template').html();
-  var template = Handlebars.compile(source);
-  return template(this);
-};
-// portSetup();
-PortfolioObject.loadAll = function(rawData){
-  rawData.forEach(function(i) {
-    portObjectArray.push(new PortfolioObject(i));
-  });
-};
-PortfolioObject.fetchAll = function(){
-  if(localStorage.rawData){
-    console.log('took this path');
-    PortfolioObject.loadAll(JSON.parse(localStorage.rawData));
-    portObjectArray.forEach(function(i) {
-      $('#portfolio').append(i.toHtml());
+(function(module) {
+
+  var portObjectArray = [];
+  function PortfolioObject(portData){
+    this.name = portData.name;
+    this.link = portData.link;
+    this.img = portData.img;
+    this.body = portData.body;
+    this.fucksGiven = portData.fucksGiven;
+    this.category = portData.category;
+    this.date = portData.date;
+  }
+
+  PortfolioObject.prototype.toHtml = function(){
+    var source = $('#portfolio-template').html();
+    var template = Handlebars.compile(source);
+    return template(this);
+  };
+  // portSetup();
+  PortfolioObject.loadAll = function(rawData){
+    rawData.map((i) => {
+      portObjectArray.push(new PortfolioObject(i));
     });
-  } else {
-    $.getJSON('javascript/data.json').then(function(rawData){
-      console.log('test test test');
-      PortfolioObject.loadAll(rawData);
-      localStorage.rawData = JSON.stringify(rawData);
-      portObjectArray.forEach(function(i) {
+  };
+  PortfolioObject.fetchAll = function(){
+    if(localStorage.rawData){
+      console.log('took this path');
+      PortfolioObject.loadAll(JSON.parse(localStorage.rawData));
+      portObjectArray.map((i) => {
         $('#portfolio').append(i.toHtml());
       });
-    }, function(err) {
-      console.error(err);
-    });
-  }
-};
+      $('#fucksGiven').append(portObjectArray.map((a) => parseInt(a.fucksGiven)).reduce((b, c) => b + c));
+    } else {
+      $.getJSON('javascript/data.json').then(function(rawData){
+        console.log('test test test');
+        PortfolioObject.loadAll(rawData);
+        localStorage.rawData = JSON.stringify(rawData);
+        portObjectArray.map((i) => {
+          $('#portfolio').append(i.toHtml());
+        });
+        $('#fucksGiven').append(portObjectArray.map((a) => parseInt(a.fucksGiven)).reduce((b, c) => b + c));
+      }, function(err) {
+        console.error(err);
+      });
+    }
+  };
 //code heavily drawn from 2nd pair programming lab
+  module.PortfolioObject = PortfolioObject;
+})(window);
